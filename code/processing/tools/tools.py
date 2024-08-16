7979a787ef68fccacb408c04e569837ebe7f09fe
kins912 <kins912@gmail.com>
2024-08-16 15:27:39 -0400

    Revisions re-organization

--- code/processing/tools/tools.py
+++ code/processing/tools/tools.py
@@ -99,9 +99,9 @@ gene_pathway_map = {
     'HOG':['HOG1','PBS2','SSK2'],
     'RTG':['RTG2','MKS1','BMH1','BMH2'],
     'TCA cycle':['CIT1','KGD1','MDH1','MAE1','ALD5'],
-    'Others':['ARO80','MKT1','GSH1'],
     # 'Deadenylation/Mitochondial Function':['PUF3','PAB1','PAN2','PAN3','AIM17'],}
-    'Deadenylation/Mitochondial Function':['PUF3','PAB1','PAN2','PAN3','AIM17']
+    'Deadenylation/Mitochondial Function':['PUF3','PAB1','PAN2','PAN3','AIM17'],
+    'Other':['ARO80','MKT1','GSH1'],
     # 'Deadenylation/Mitochondial Function':['PAB1','PAN2','PAN3','AIM17']
     }
 
